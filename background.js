e1a1863d2b868ae0d38e8d0a171ca9ccccb9f695
crazytimon <crazytimon88@gmail.com>
2023-03-25 23:39:47 +0100

    add new prompt features

--- background.js
+++ background.js
@@ -18,7 +18,35 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create({
     id: 'gpt-emoji-suggest-item2',
     parentId: 'gpt-emoji-suggest-root',
-    title: 'Новая функция',
+    title: 'Сделать текст более вежливым',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    id: 'gpt-emoji-suggest-item3',
+    parentId: 'gpt-emoji-suggest-root',
+    title: 'Сделать текст более агресивным',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    id: 'gpt-emoji-suggest-item4',
+    parentId: 'gpt-emoji-suggest-root',
+    title: 'Сделать текст более мотивирующим',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    id: 'gpt-emoji-suggest-item5',
+    parentId: 'gpt-emoji-suggest-root',
+    title: 'Сделать название для статьи',
+    contexts: ['selection'],
+  });
+
+  chrome.contextMenus.create({
+    id: 'gpt-emoji-suggest-item6',
+    parentId: 'gpt-emoji-suggest-root',
+    title: 'Краткое описание текста',
     contexts: ['selection'],
   });
 });
@@ -27,6 +55,14 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === 'gpt-emoji-suggest-item1') {
     chrome.tabs.sendMessage(tab.id, {command: 'fetchSelectedTextAndDisplayEmojis'});
   } else if (info.menuItemId === 'gpt-emoji-suggest-item2') {
-    chrome.tabs.sendMessage(tab.id, {command: 'customNewFunction'});
+    chrome.tabs.sendMessage(tab.id, {command: 'polite'});
+  } else if (info.menuItemId === 'gpt-emoji-suggest-item3') {
+    chrome.tabs.sendMessage(tab.id, {command: 'aggresive'});
+  } else if (info.menuItemId === 'gpt-emoji-suggest-item4') {
+    chrome.tabs.sendMessage(tab.id, {command: 'motivation'});
+  } else if (info.menuItemId === 'gpt-emoji-suggest-item5') {
+    chrome.tabs.sendMessage(tab.id, {command: 'articleTitle'});
+  } else if (info.menuItemId === 'gpt-emoji-suggest-item6') {
+    chrome.tabs.sendMessage(tab.id, {command: 'summary'});
   }
 });
--- content-script.js
+++ content-script.js
@@ -1,8 +1,8 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.command === 'fetchSelectedTextAndDisplayEmojis') {
     fetchSelectedTextAndDisplayEmojis();
-  } else if (message.command === 'customNewFunction') {
-    customNewFunction();
+  } else {
+    proccesMessage(message.command);
   }
 });
 
@@ -25,6 +25,71 @@ async function fetchSelectedTextAndDisplayEmojis() {
   }
 }
 
+async function proccesMessage(command) {
+  const selectedText = window.getSelection().toString();
+
+  if (selectedText) {
+
+      const selectedText = window.getSelection().toString().trim();
+
+      if (selectedText) {
+        const language = (await chrome.storage.sync.get('language')).language || 'English';
+        const apikey = (await chrome.storage.sync.get('apikey')).apikey;
+        let prompt = '';
+        switch (command) {
+          case 'polite':
+            prompt = `Given the text "${selectedText}" makes it more polite. Awnser in ${language}`
+            break;
+          case 'aggresive':
+            prompt = `Given the text "${selectedText}" makes it more aggresive. Awnser in ${language}`
+            break;
+          case 'motivation':
+            prompt = `Given the text "${selectedText}" makes it more motivated. Awnser in ${language}`
+            break;
+          case 'articleTitle':
+            prompt = `Make a title for article:"${selectedText}". Awnser in ${language}`
+            break;
+          case 'summary':
+            prompt = `Make summary for "${selectedText}". Awnser in ${language}`
+            break;
+          default:
+            break;
+        }
+        const gptAnswer = await fetchProccesedMessage(apikey, prompt);
+        showPopup(gptAnswer);
+      }
+  }
+}
+
+async function fetchProccesedMessage(apikey, prompt) {
+  const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apikey}`
+      },
+      body: JSON.stringify({
+        prompt: prompt,
+        max_tokens: 500,
+        n: 1,
+        stop: null,
+        temperature: 0.5
+      })
+    });
+
+    const data = await response.json();
+    const text = data.choices[0].text;
+
+    return text;
+  } catch (error) {
+    console.error('Error fetching emoji suggestions:', error);
+    return [];
+  }
+}
+
 async function fetchEmojiSuggestions(text, language, apikey) {
   const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
 
@@ -39,7 +104,7 @@ async function fetchEmojiSuggestions(text, language, apikey) {
       },
       body: JSON.stringify({
         prompt: prompt,
-        max_tokens: 500,
+        max_tokens: 750,
         n: 1,
         stop: null,
         temperature: 0.5
@@ -118,6 +183,42 @@ function displayEmojis(emojiInfo) {
   document.body.appendChild(emojiPopup);
 }
 
-async function customNewFunction() {
-  // реализуйте новую функцию здесь
+
+function showPopup(text) {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const rect = range.getBoundingClientRect();
+
+  const emojiPopup = document.createElement('div');
+  emojiPopup.style.position = 'fixed';
+  emojiPopup.style.top = `${rect.top + window.scrollY - 50}px`;
+  emojiPopup.style.left = `${rect.left + window.scrollX}px`;
+  emojiPopup.style.backgroundColor = '#ffffff';
+  emojiPopup.style.border = '1px solid #cccccc';
+  emojiPopup.style.borderRadius = '5px';
+  emojiPopup.style.padding = '10px';
+  emojiPopup.style.zIndex = '9999';
+  emojiPopup.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+  emojiPopup.style.width = '500px';
+  emojiPopup.textContent = text;
+
+  const closeButton = document.createElement('button');
+  closeButton.textContent = '×';
+  closeButton.style.position = 'absolute';
+  closeButton.style.top = '5px';
+  closeButton.style.right = '5px';
+  closeButton.style.border = 'none';
+  closeButton.style.background = 'none';
+  closeButton.style.fontSize =
+    '18px';
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.padding = '0';
+  closeButton.style.lineHeight = '1';
+  closeButton.style.color = '#999999';
+  closeButton.addEventListener('click', () => {
+    emojiPopup.remove();
+  });
+
+  emojiPopup.appendChild(closeButton);
+  document.body.appendChild(emojiPopup);
 }
