f6eb374987fdadadd6c68875b565e2364fd9500b
crazytimon <crazytimon88@gmail.com>
2023-03-25 20:07:45 +0100

    better styles and bettrer prompt

--- content-script.js
+++ content-script.js
@@ -1,49 +1,115 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'FETCH_EMOJIS') {
-      const selectedText = window.getSelection().toString();
-  
-      if (selectedText) {
-        sendResponse({ success: true, selectedText });
-      } else {
-        sendResponse({ success: false });
-      }
-    } else if (message.type === 'SHOW_EMOJIS') {
-      showEmojiPopup(message.emojis);
+  if (message.command === 'fetchSelectedTextAndDisplayEmojis') {
+    fetchSelectedTextAndDisplayEmojis();
+  } else if (message.command === 'customNewFunction') {
+    customNewFunction();
+  }
+});
+
+async function fetchSelectedTextAndDisplayEmojis() {
+  const selectedText = window.getSelection().toString();
+
+  if (selectedText) {
+    const emojis = await fetchEmojiSuggestions(selectedText);
+
+    if (emojis) {
+      displayEmojis(emojis);
     }
+  }
+}
+
+async function fetchEmojiSuggestions(text) {
+  const apiKey = ''; // Замените на ваш API-ключ
+  const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
+
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${apiKey}`
+      },
+      body: JSON.stringify({
+        prompt: `Given the text "${text}", suggest three emojis (unicode characters) that are related to the text, along with a brief explanation for each.`,
+        max_tokens: 100,
+        n: 1,
+        stop: null,
+        temperature: 0.5
+      })
+    });
+
+    const data = await response.json();
+    const completion = data.choices[0].text;
+    const emojiInfo = completion.trim().split('\n').slice(0, 3).map(info => {
+      const [emojiWithLabel, explanation] = info.split(': ');
+      const emoji = emojiWithLabel.replace(/ *\([^)]*\) */, '');
+      return { emoji, explanation };
+    });
+
+    return emojiInfo;
+  } catch (error) {
+    console.error('Error fetching emoji suggestions:', error);
+    return [];
+  }
+}
+
+
+function displayEmojis(emojiInfo) {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const rect = range.getBoundingClientRect();
+
+  const emojiPopup = document.createElement('div');
+  emojiPopup.style.position = 'fixed';
+  emojiPopup.style.top = `${rect.top + window.scrollY - 50}px`;
+  emojiPopup.style.left = `${rect.left + window.scrollX}px`;
+  emojiPopup.style.backgroundColor = '#ffffff';
+  emojiPopup.style.border = '1px solid #cccccc';
+  emojiPopup.style.borderRadius = '5px';
+  emojiPopup.style.padding = '10px';
+  emojiPopup.style.zIndex = '9999';
+  emojiPopup.style.boxShadow = '0 2px 10px rgba(0, 0, 0, 0.1)';
+  emojiPopup.style.width = '300px';
+
+  emojiInfo.forEach(({ emoji, explanation }) => {
+    const emojiElement = document.createElement('span');
+    emojiElement.textContent = emoji;
+    emojiElement.style.fontSize = '28px';
+    emojiElement.style.marginRight = '8px';
+
+    const explanationElement = document.createElement('span');
+    explanationElement.textContent = explanation;
+    explanationElement.style.fontSize = '14px';
+
+    const emojiContainer = document.createElement('div');
+    emojiContainer.style.marginBottom = '5px';
+    emojiContainer.appendChild(emojiElement);
+    emojiContainer.appendChild(explanationElement);
+
+    emojiPopup.appendChild(emojiContainer);
   });
 
-  function showEmojiPopup(emojis) {
-    const popup = document.createElement('div');
-    popup.classList.add('emoji-popup');
-    popup.innerHTML = emojis.join(' ');
-  
-    const selection = window.getSelection();
-    const range = selection.getRangeAt(0);
-    const rect = range.getBoundingClientRect();
-  
-    popup.style.position = 'fixed';
-    popup.style.left = `${rect.left + window.scrollX}px`;
-    popup.style.top = `${rect.top + window.scrollY - 40}px`;
-  
-    document.body.appendChild(popup);
-  
-    setTimeout(() => {
-      document.body.removeChild(popup);
-    }, 10000);
-  }
-  
-const emojiPopupStyles = `
-  .emoji-popup {
-    background-color: white;
-    border: 1px solid black;
-    border-radius: 4px;
-    padding: 8px;
-    font-size: 24px;
-    z-index: 9999;
-    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1);
-  }
-`;
+  const closeButton = document.createElement('button');
+  closeButton.textContent = '×';
+  closeButton.style.position = 'absolute';
+  closeButton.style.top = '5px';
+  closeButton.style.right = '5px';
+  closeButton.style.border = 'none';
+  closeButton.style.background = 'none';
+  closeButton.style.fontSize =
+    '18px';
+  closeButton.style.cursor = 'pointer';
+  closeButton.style.padding = '0';
+  closeButton.style.lineHeight = '1';
+  closeButton.style.color = '#999999';
+  closeButton.addEventListener('click', () => {
+    emojiPopup.remove();
+  });
 
-const styleElement = document.createElement('style');
-styleElement.textContent = emojiPopupStyles;
-document.head.appendChild(styleElement);
+  emojiPopup.appendChild(closeButton);
+  document.body.appendChild(emojiPopup);
+}
+
+async function customNewFunction() {
+  // реализуйте новую функцию здесь
+}
